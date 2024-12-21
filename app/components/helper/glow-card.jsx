29b18e2e42653f905a9779604b8fb889a5867f49
baisalikhan <140602823+Baisexodevs@users.noreply.github.com>
2024-12-21 14:33:41 +0500

    fix: make the glow-card compatible wih server side rendering

--- app/components/helper/glow-card.jsx
+++ app/components/helper/glow-card.jsx
@@ -1,81 +1,9 @@
-"use client"
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
+import GlowEffect from './glow-effect';
 
 const GlowCard = ({ children, identifier }) => {
   const containerRef = useRef(null);
   const cardRef = useRef(null);
-
-  useEffect(() => {
-    if (typeof window === 'undefined') return;
-    
-    const CONTAINER = containerRef.current;
-    const CARDS = [cardRef.current];
-
-    const CONFIG = {
-      proximity: 40,
-      spread: 80,
-      blur: 12,
-      gap: 32,
-      vertical: false,
-      opacity: 0,
-    };
-
-    const UPDATE = (event) => {
-      if (!CONTAINER || !CARDS[0]) return;
-
-      for (const CARD of CARDS) {
-        const CARD_BOUNDS = CARD.getBoundingClientRect();
-
-        if (
-          event?.x > CARD_BOUNDS.left - CONFIG.proximity &&
-          event?.x < CARD_BOUNDS.left + CARD_BOUNDS.width + CONFIG.proximity &&
-          event?.y > CARD_BOUNDS.top - CONFIG.proximity &&
-          event?.y < CARD_BOUNDS.top + CARD_BOUNDS.height + CONFIG.proximity
-        ) {
-          CARD.style.setProperty('--active', 1);
-        } else {
-          CARD.style.setProperty('--active', CONFIG.opacity);
-        }
-
-        const CARD_CENTER = [
-          CARD_BOUNDS.left + CARD_BOUNDS.width * 0.5,
-          CARD_BOUNDS.top + CARD_BOUNDS.height * 0.5,
-        ];
-
-        let ANGLE =
-          (Math.atan2(event?.y - CARD_CENTER[1], event?.x - CARD_CENTER[0]) *
-            180) /
-          Math.PI;
-
-        ANGLE = ANGLE < 0 ? ANGLE + 360 : ANGLE;
-
-        CARD.style.setProperty('--start', ANGLE + 90);
-      }
-    };
-
-    const RESTYLE = () => {
-      if (!CONTAINER) return;
-      CONTAINER.style.setProperty('--gap', CONFIG.gap);
-      CONTAINER.style.setProperty('--blur', CONFIG.blur);
-      CONTAINER.style.setProperty('--spread', CONFIG.spread);
-      CONTAINER.style.setProperty(
-        '--direction',
-        CONFIG.vertical ? 'column' : 'row'
-      );
-    };
-
-    if (typeof window !== 'undefined') {
-      window.addEventListener('pointermove', UPDATE);
-      RESTYLE();
-      UPDATE();
-    }
-
-    return () => {
-      if (typeof window !== 'undefined') {
-        window.removeEventListener('pointermove', UPDATE);
-      }
-    };
-  }, [identifier]);
 
   return (
     <div ref={containerRef} className={`glow-container-${identifier} glow-container`}>
@@ -83,6 +11,11 @@ const GlowCard = ({ children, identifier }) => {
         <div className="glows"></div>
         {children}
       </article>
+      <GlowEffect 
+        containerRef={containerRef}
+        cardRef={cardRef}
+        identifier={identifier}
+      />
     </div>
   );
 };
